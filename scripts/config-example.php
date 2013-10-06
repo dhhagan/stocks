55a0c68e19c862835defd66374c850296fc59bf2
David H Hagan <david@davidhhagan.com>
2013-10-05 21:45:42 -0400

    added example config file

---
+++ scripts/config-example.php
@@ -0,0 +1,8 @@
+<?php
+//Configuration information:: Should be ignored with .gitignore
+define("HOST","localhost");
+define("USERNAME","david");
+define("PASSWORD","a8041807");
+define("DATABASE","stocks");
+
+?>
